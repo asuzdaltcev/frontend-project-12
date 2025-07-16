c18dd3a0a2368372579678bdab49503f9dacca13
Aleksandr Suzdaltcev <rodriguezzz@gmail.com>
2025-07-16 13:41:36 -0700

    Обновление конфигурации сборки: добавлен скрипт для простого билда, обновлены зависимости Babel и Vite, добавлены параметры для Rollup и глобальной переменной.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,4 +9,12 @@ export default defineConfig({
       '/api': 'http://localhost:5001',
     },
   },
+  build: {
+    rollupOptions: {
+      external: ['crypto'],
+    },
+  },
+  define: {
+    global: 'globalThis',
+  },
 })
