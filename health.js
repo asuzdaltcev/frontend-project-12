2f8336edfe84f92ef3f29926be33be904ca1e9f7
Aleksandr Suzdaltcev <rodriguezzz@gmail.com>
2025-07-31 19:29:46 -0700

    Исправление форматирования кода в файле health.js. Упрощение логики обработки запросов и добавление переноса строки в конце файла.

--- health.js
+++ health.js
@@ -5,7 +5,8 @@ const server = http.createServer((req, res) => {
   if (req.url === '/health') {
     res.writeHead(200, { 'Content-Type': 'application/json' })
     res.end(JSON.stringify({ status: 'ok', timestamp: new Date().toISOString() }))
-  } else {
+  }
+  else {
     res.writeHead(404)
     res.end('Not Found')
   }
